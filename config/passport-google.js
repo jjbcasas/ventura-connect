6e04b24f20ee65e5f44d6715cd1033a7928cb9c6
John Joseph Casas <casasjohnjoseph@gmail.com>
2025-05-07 22:05:55 -0700

    made some changes

---
+++ config/passport-google.js
@@ -0,0 +1,51 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
+const User = require('../models/User')
+const passport = require('passport')
+
+module.exports = function(passport){
+    passport.use(new GoogleStrategy({
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: 'http://localhost:2121/auth/google/callback',
+        // passReqToCallback: true
+    },
+    async ( accessToken, refreshToken, profile, cb) => {
+        try {
+            // Check if the user already exists based on their Google ID
+            let user = await User.findOne({ googleId: profile.id })
+            if (user) {
+                return done(null, user);
+              } else {
+                // If the user doesn't exist, create a new user in your database
+                const user = new User({
+                  googleId: profile.id,
+                  userName: profile.displayName,
+                  email: profile.emails ? profile.emails[0].value : null,
+                  // You might want to store other profile information as needed
+                });
+                const savedUser = await user.save();
+                return done(null, savedUser);
+              }
+            } catch (err) {
+              return done(err);
+            }
+        // }
+        // User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        //     return cb(err, user);
+        //   })
+    }
+    ))
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+      
+    passport.deserializeUser(async (id, done) => {
+        try {
+            const user = await User.findById(id);
+            done(null, user);
+        } catch (err) {
+            done(err, null);
+        }
+    });
+}
--- routes/main.js
+++ routes/main.js
@@ -11,7 +11,7 @@ router.post('/login', authController.postLogin)
 router.get('/logout', authController.logout)
 router.get('/signup', authController.getSignup)
 router.post('/signup', authController.postSignup)
-router.get('/auth/google', passport.authenticate('google', { scope: ['email','profile'] }))
+router.get('/auth/google', () => console.log('Google auth route hit!'), passport.authenticate('google', { scope: ['email','profile'] }))
 router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), authController.googleCallback)
 
 module.exports = router
