d0297ec4168b8bd762e06176c98f83568c328046
John Joseph Casas <casasjohnjoseph@gmail.com>
2025-05-30 15:18:24 -0700

    made some changes

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express')
+// initializes express
 const app = express()
+// requiring mongoose to connect to mongodb.
 const mongoose = require('mongoose')
+// package that helps us with our authentication or login
 const passport = require('passport')
+// to make a session for our users to stay logged in and then store our session info to mongodb
+// express-session creates the cookie
 const session = require('express-session')
+// mongostore stores the session object to our mongodb
 const MongoStore = require('connect-mongo')/*(session)*/
+// let us override the form and use put and delete request on it
 const methodOverride = require('method-override')
+// it shows alert msg when logging in/verification is a success or an error
 const flash = require('express-flash')
+// helps us log everything thats happening. to see all the requests coming thru
 const logger = require('morgan')
 const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
@@ -13,6 +22,7 @@ const feedRoutes = require('./routes/feed')
 const profileRoutes = require('./routes/profile')
 const postRoutes = require('./routes/post')
 
+// use .env file in config folder
 require('dotenv').config({path:'./config/.env'})
 
 // Passport config
@@ -21,20 +31,26 @@ require('./config/passport')(passport)
 // Passport google-oauth20 config
 require('./config/passport-google')(passport)
 
+// connect to the Database
 connectDB()
 
+// using ejs for views
 app.set('view engine','ejs')
 
+// static folder
 app.use(express.static('public'))
 
+// body parsing, so we can pull something from the request
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
+// logging
 app.use(logger('dev'))
 
+// use forms for put / delete
 app.use(methodOverride('_method'))
 
-// Sessions
+// setup sessions - stored in mongodb
 app.use(
     session({
         secret: 'keyboard cat',
@@ -51,13 +67,16 @@ app.use(
 app.use(passport.initialize())
 app.use(passport.session())
 
+// use flash messages for errors, info, etc.
 app.use(flash())
 
+// set up routes
 app.use('/',mainRoutes)
 app.use('/feed', feedRoutes)
 app.use('/profile', profileRoutes)
 app.use('/post', postRoutes)
 
+// server running
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}, you better catch it`)
 })
